e26c39672ecf204e2cec8a2c43610b761e8e87d9
joyfan00 <joy.fan00@gmail.com>
2024-01-30 01:42:00 -0500

    numba changes to DSTP- not working

--- DSTP.py
+++ DSTP.py
@@ -2,6 +2,7 @@
 import numpy as np
 import random
 import pandas as pd
+import numba as nb
 from file_input import *
 from Model import Model
 from variables import Variables
@@ -25,8 +26,8 @@ class DSTP(Model):
         self.bounds = [(0,10),(0,1),(0,1),(0,10),(0,1),(0,1),(0,1),(0,2),(0,min(self.data['rt']))]
         super().__init__(self.param_number, self.bounds, self.parameter_names)
 
-    @staticmethod
-    def model_simulation(alphaSS, betaSS, deltaSS, alphaRS, betaRS1, delta_target, delta_flanker, deltaRS2, tau, dt=0.001, var=.1, nTrials=5000, noiseseed=0):
+    @nb.jit(nopython=True, cache=True, parallel=False, fastmath=True, nogil=True)
+    def model_simulation(alphaSS, betaSS, deltaSS, alphaRS, betaRS1, delta_target, delta_flanker, deltaRS2, tau, dt=Variables.DT, var=Variables.VAR, nTrials=Variables.NTRIALS, noiseseed=Variables.NOISESEED):
         """
         Performs simulations for DMC model.
         @parameters (dict): contains all variables and associated values for DSTP models- 
@@ -37,21 +38,20 @@ class DSTP(Model):
         @noiseseed (int): random seed for noise consistency
         """
 
-        choicelist = []
-        rtlist = []
+        choicelist = [np.nan]*nTrials
+        rtlist = [np.nan]*nTrials
         # sample a bunch of possible updates to evidence with each unit of time dt. Updates are centered
         # around drift rate delta (scaled to the units of time), with variance var (because updates are noisy)
 
-        np.random.seed(Variables.NOISESEED)
-        update_jitter = np.random.normal(loc=0, scale=Variables.VAR, size=10000)
+        np.random.seed(noiseseed)
+        update_jitter = np.random.normal(loc=0, scale=var, size=10000)
 
         # updatesRS1 = np.random.normal(loc=deltaRS1*dt, scale=var, size=10000)
         ### Add congruent list (make first half congruent)
-        congruencylist = ['congruent']*int(Variables.NTRIALS/2) + ['incongruent']*int(Variables.NTRIALS/2) 
+        congruencylist = ['congruent']*int(nTrials//2) + ['incongruent']*int(nTrials//2) 
         #deltaRS2 is always positive. Later in the model, you should either keep it positive
         ##### if the target bound is selected by SS, or multiply it by -1 if the flanker bound is selected by SS.
-        iter=0
-        for n in range(0, Variables.NTRIALS):
+        for n in np.arange(0, nTrials):
             # Assuming first half of trials are congruent
             if congruencylist[n] == 'congruent':
                 deltaRS1 = delta_target + delta_flanker
@@ -61,17 +61,16 @@ class DSTP(Model):
             evidenceSS = betaSS*alphaSS/2 - (1-betaSS)*alphaSS/2 # start our evidence at initial-bias beta (Kyle: I modified this so beta is always between 0 and 1, and alpha is the total distance between bounds)
             evidenceRS1 = betaRS1*alphaRS/2 - (1-betaRS1)*alphaRS/2
             while (evidenceSS < alphaSS/2 and evidenceSS > -alphaSS/2) or (evidenceRS1 < alphaRS/2 and evidenceRS1 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                random.seed(100+iter)
-                evidenceSS += deltaSS*Variables.DT + random.choice(update_jitter) # add one of the many possible updates to evidence
-                evidenceRS1 += deltaRS1*Variables.DT + random.choice(update_jitter)
-                t += Variables.DT # increment time by the unit dt
-                iter += 1
+                np.random.seed(n)
+                evidenceSS += deltaSS*dt + np.random.choice(update_jitter) # add one of the many possible updates to evidence
+                evidenceRS1 += deltaRS1*dt + np.random.choice(update_jitter)
+                t += dt # increment time by the unit dt
             if evidenceRS1 > alphaRS/2:
-                choicelist.append(1) # choose the upper threshold action
-                rtlist.append(t)
+                choicelist[n] = 1 # choose the upper threshold action
+                rtlist[n] = t
             elif evidenceRS1 < -alphaRS/2:
-                choicelist.append(0) # choose the lower threshold action
-                rtlist.append(t)
+                choicelist[n] = 0 # choose the lower threshold action
+                rtlist[n] = t
 
             # If SS completes before RS1, begin RS2 from where RS1 left off (but update drift rate/other params)
             else:
@@ -82,15 +81,14 @@ class DSTP(Model):
                 evidenceRS2 = evidenceRS1 # start where you left off from RS1
                 ## We weren't sure if the decision threshold is the same across both response selection phases
                 while (evidenceRS2 < alphaRS/2 and evidenceRS2 > -alphaRS/2): # keep accumulating evidence until you reach a threshold
-                    random.seed(100+iter)
-                    evidenceRS2 += deltaRS2*Variables.DT + random.choice(update_jitter)
-                    t += Variables.DT # increment time by the unit dt
-                    iter += 1
+                    np.random.seed(n)
+                    evidenceRS2 += deltaRS2*dt + np.random.choice(update_jitter)
+                    t += dt # increment time by the unit dt
                 if evidenceRS2 > alphaRS/2:
-                    choicelist.append(1) # choose the upper threshold action
-                    rtlist.append(t)
+                    choicelist[n] = 1 # choose the upper threshold action
+                    rtlist[n] = t
                 elif evidenceRS2 < -alphaRS/2:
-                    choicelist.append(0) # choose the lower threshold action
-                    rtlist.append(t)
-        return (range(1, Variables.NTRIALS+1), choicelist, rtlist, congruencylist)
+                    choicelist[n] = 0 # choose the lower threshold action
+                    rtlist[n] = t
+        return (np.arange(1, nTrials+1), choicelist, rtlist, congruencylist)
     
--- SSP.py
+++ SSP.py
@@ -3,6 +3,7 @@ from Model import Model
 from variables import Variables
 from file_input import *
 # import pandas as pd
+import numba as nb
 import numpy as np
 import math
 import random
@@ -30,7 +31,8 @@ class SSP(Model):
         self.bounds = [(0,1),(0,1),(0,1),(0,3),(0,1),(0,min(self.data['rt']))]
         super().__init__(self.param_number, self.bounds, self.parameter_names)
     
-    def model_simulation(self, alpha, beta, p, sd_0, sd_r, tau, dt=0.001, var=.1, nTrials=5000, noiseseed=0):
+    @nb.jit(nopython=True, cache=True, parallel=False, fastmath=True, nogil=True)
+    def model_simulation(self, alpha, beta, p, sd_0, sd_r, tau, dt=Variables.DT, var=Variables.VAR, nTrials=Variables.NTRIALS, noiseseed=Variables.NOISESEED):
         """
         Performs simulations for DMC model.
         @parameters (dict): contains all variables and associated values for DMC models- 
@@ -40,29 +42,19 @@ class SSP(Model):
         @nTrials (int): number of trials
         @noiseseed (int): random seed for noise consistency
         """
-        # quick check to make sure that dictionary is inputted correctly 
-        # if (len(parameters) != self.param_number):
-        #     print('Dictionary input is not correct.')
 
-        # # define variables f
-        # alpha = parameters['alpha']
-        # beta = parameters['beta']
-        # p = parameters['p']
-        # sd_0 = parameters['sd_0']
-        # sd_r = parameters['sd_r']
-        # tau = parameters['tau']
-
-        # 
         scale = 10000
-        choicelist = []
-        rtlist = []
-        congruencylist = ['congruent']*int(Variables.NTRIALS/2) + ['incongruent']*int(Variables.NTRIALS/2) 
-        np.random.seed(Variables.NOISESEED)
-        noise = np.random.normal(loc=0, scale=Variables.VAR, size=scale)
-        tlist = np.array(np.arange(0, Variables.DT*scale, Variables.DT))
+        choicelist = [np.nan]*nTrials
+        rtlist = [np.nan]*nTrials
+
+        congruencylist = ['congruent']*int(nTrials//2) + ['incongruent']*int(nTrials//2) 
+        np.random.seed(noiseseed)
+        noise = np.random.normal(loc=0, scale=var, size=scale)
+
+        tlist = np.array(np.arange(0, dt*scale, dt))
         s_ta = np.apply_along_axis(lambda x: SSP.sdfunc(x, sd_0, sd_r), 0, tlist)
         s_fl = np.apply_along_axis(lambda x: SSP.fastsub(x, s_ta), 0, [1]*scale)
-        for n in range(0, Variables.NTRIALS):
+        for n in np.arange(0, nTrials):
             tind = 0
             np.random.shuffle(noise)
             delta_ta = np.apply_along_axis(lambda x: SSP.fastmult(x, [p]*scale), 0, s_ta)
@@ -71,7 +63,7 @@ class SSP(Model):
                 delta = np.apply_along_axis(lambda x: SSP.fastsub(x, delta_fl), 0, delta_ta)
             else:
                 delta = np.apply_along_axis(lambda x: SSP.fastadd(x, delta_fl), 0, delta_ta)
-            changelist = np.apply_along_axis(lambda x: SSP.fastmult(x, [Variables.DT]*scale), 0, delta)
+            changelist = np.apply_along_axis(lambda x: SSP.fastmult(x, [dt]*scale), 0, delta)
             evidencelist = list(np.apply_along_axis(lambda x: SSP.fastadd(x, noise), 0, changelist))
             evidencelist.insert(0, beta*alpha/2 - (1-beta)*alpha/2)
             cumulative_evidence = np.cumsum(evidencelist)
@@ -81,8 +73,8 @@ class SSP(Model):
                 tind += scale
                 oldevidence = cumulative_evidence[-1]
                 np.random.shuffle(noise)
-                tlist = np.arange(Variables.DT*tind, Variables.DT*(tind+scale), Variables.DT)
-                if (sd_0 - (sd_r * (Variables.DT*tind))) <= 0.001:
+                tlist = np.arange(dt*tind, dt*(tind+scale), dt)
+                if (sd_0 - (sd_r * (dt*tind))) <= 0.001:
                     s_ta = [norm(0, 0.001).cdf(.5) - norm(0, 0.001).cdf(-.5)]*scale
                 else:
                     s_ta = np.apply_along_axis(lambda x: SSP.sdfunc(x, sd_0, sd_r), 0, tlist)
@@ -93,7 +85,7 @@ class SSP(Model):
                     delta = np.apply_along_axis(lambda x: SSP.fastsub(x, delta_fl), 0, delta_ta)
                 else:
                     delta = np.apply_along_axis(lambda x: SSP.fastadd(x, delta_fl), 0, delta_ta)
-                changelist = np.apply_along_axis(lambda x: SSP.fastmult(x, [Variables.DT]*scale), 0, delta)
+                changelist = np.apply_along_axis(lambda x: SSP.fastmult(x, [dt]*scale), 0, delta)
                 evidencelist = list(np.apply_along_axis(lambda x: SSP.fastadd(x, noise), 0, changelist))
                 evidencelist.insert(0, oldevidence)
                 cumulative_evidence = np.cumsum(evidencelist)
@@ -108,10 +100,10 @@ class SSP(Model):
                 choicelist.append(1)
             else:
                 choicelist.append(0)
-            t = (min(above, below) + tind)*Variables.DT + tau
+            t = (min(above, below) + tind)*dt + tau
             rtlist.append(t)
 
-        return (range(1, Variables.NTRIALS+1), choicelist, rtlist, congruencylist)
+        return (np.arange(1, nTrials+1), choicelist, rtlist, congruencylist)
     
     @staticmethod
     def sdfunc(x, sd_0, sd_r):
