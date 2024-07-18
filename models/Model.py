b1ef89249f6b49d2e33d6c63f69d05dcc9318fe1
Kyle LaFollette <32970290+kjlafoll@users.noreply.github.com>
2024-07-18 00:21:59 -0400

    update

--- models/Model.py
+++ models/Model.py
@@ -64,8 +64,8 @@ class Model:
                         method='Nelder-Mead')
         else:
             fit = differential_evolution(Model.model_function, bounds=bounds_var, x0=params,
-                                    args=(props,self.param_number,self.parameter_names, function, data, bounds_var), maxiter=100, seed=10,
-                                    disp=True, popsize=8, polish=True, callback=printCurrentIteration, workers=-1)
+                                    args=(props,self.param_number,self.parameter_names, function, data, bounds_var), maxiter=1000, seed=10,
+                                    disp=True, popsize=12, polish=True, callback=printCurrentIteration, workers=-1)
             # popsize = 100, maxiter = 1000
         bestparams = fit.x
         fitstat = fit.fun
